9cfeea423e6fd34c82ad8651911a99538ab85459
IstvanBruno <bruno.istvan@gmail.com>
2018-09-29 14:51:12 -0300

    Inclusão de data na classe modelo de Nota

--- src/main/kotlin/br/com/bicmsystems/notepad/model/Nota.kt
+++ src/main/kotlin/br/com/bicmsystems/notepad/model/Nota.kt
@@ -2,11 +2,13 @@ package br.com.bicmsystems.notepad.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.Date
 
 @Document(collection = "Notas")
 data class Nota (
         @Id var id: String?,
         var titulo: String = "",
-        var descricao: String = ""
+        var descricao: String = "",
+        var data: Date = Date()
 ) {
 }
